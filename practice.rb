adbb857de794bd96822cb6f3bb43cb678ccd2235
Ben Hand <gneeshot@gmail.com>
2015-09-10 10:40:15 -0500

    added in purpose of file in comments

---
+++ practice.rb
@@ -0,0 +1,4 @@
+# Practice
+# Create a new branch that is independent of any work project, and try and add some comments on there
+# Add and commit some comments (not code!) and see how resetting works at different stages
+# #comment 1,
