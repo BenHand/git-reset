505721d3bf7dcaca9f20beeceef7be5a28156b6d
Ben Hand <gneeshot@gmail.com>
2015-09-10 10:44:17 -0500

    comment # 2 inserted

--- practice.rb
+++ practice.rb
@@ -2,3 +2,4 @@
 # Create a new branch that is independent of any work project, and try and add some comments on there
 # Add and commit some comments (not code!) and see how resetting works at different stages
 # #comment 1,
+# comment 2
